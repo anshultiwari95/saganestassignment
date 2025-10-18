ff93ac843793e536f651b76146f0ac084e1da7ed
Anshul Tiwari <anshul.tiwari1223@gmail.com>
2025-10-18 14:32:35 +0530

    Logic Updation

--- src/components/header/Headers.jsx
+++ src/components/header/Headers.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Button from '../button/Button.jsx'
 
 export const Headers = () => {
-  console.log('Headers component is rendering...');
   return (
     <header className="bg-white shadow-sm border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -11,12 +11,6 @@ export { default as Footer } from './footer/Footer.jsx';
 export { default as Client } from './sections/Client.jsx';
 export { default as CommunityManagement } from './sections/CommunityManagement.jsx';
 export { default as Spending } from './sections/Spending.jsx';
-export { default as AboutSection } from './sections/AboutSection.jsx';
-export { default as ServicesSection } from './sections/ServicesSection.jsx';
-export { default as ContactSection } from './sections/ContactSection.jsx';
 
 // UI Components
 export { default as Button } from './button/Button.jsx';
-
-// Examples
-export { default as ButtonExamples } from './examples/ButtonExamples.jsx';
